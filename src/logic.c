f2a2a1fd1385d930f995be927bb10dc3bac07025
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-03 18:43:02 -0500

    Frustration mounts but I will not yield to it

--- src/logic.c
+++ src/logic.c
@@ -11,16 +11,22 @@
 static Character **characters; // array of character pointers
 static int character_count;
 
+static Sprite *waffle_cone;
+
 void logic_init() {
 
 	load_mods(&characters, &character_count);
+
+	waffle_cone = load_sprite("src/waffle_cone.png");
 }
 
 void logic_tick(const unsigned long time) {
 
 	// toppings
 
-	draw_character(10, 10, characters[0], 1, EMOTION_NEUTRAL);
+	draw_character(10, 10, characters[0], (time / 100) % 3, EMOTION_NEUTRAL);
+
+	draw_sprite(waffle_cone, 0, 0, FALSE);
 }
 
 void logic_click(const int x, const int y) {
