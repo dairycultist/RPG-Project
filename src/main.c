d21e21f7252f684035f20463cca444a88d8ad36d
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-01-27 22:05:07 -0500

    Plan room data format

--- src/main.c
+++ src/main.c
@@ -8,11 +8,14 @@ int main() {
 
 	AbstractWindow *window = create_window("Test", 240, 160, 60);
 
-	AbstractSprite *sprite = load_sprite(window, "./res/card.png");
+	AbstractSprite *sprite = load_sprite(window, "./res/snivy.png");
 	
+	int t = 0;
+
 	while (1) {
-		draw_sprite(window, sprite, 0, 0, 0);
+		draw_subsprite(window, sprite, 0, 0, (t / 8 % 4) * 32, (t / 32 % 8) * 32, 32, 32, 0);
 		present(window);
+		t++;
 	}
 
 	return 0;
